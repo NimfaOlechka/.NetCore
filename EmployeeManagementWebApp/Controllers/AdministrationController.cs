b68d6ce1977fc4b946bb408939e6757b59266987
Olga K <nimfaolya@mail.ru>
2021-04-22 20:25:02 +0200

    - added role management function - changed rules to show navigation menu option for role list and creating users

--- EmployeeManagementWebApp/Controllers/AdministrationController.cs
+++ EmployeeManagementWebApp/Controllers/AdministrationController.cs
@@ -1,5 +1,6 @@
 ﻿using EmployeeManagementWebApp.Models;
 using EmployeeManagementWebApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace EmployeeManagementWebApp.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
@@ -111,6 +113,95 @@ namespace EmployeeManagementWebApp.Controllers
                 return View(model);
             }
 
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditUsersInRole(string roleId)
+        {
+            ViewBag.roleId = roleId;
+
+            var role = await roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} can't be found";
+                return View("NotFound");
+            }
+
+            var model = new List<UserRoleViewModel>();
+            foreach(var user in userManager.Users)
+            {
+                var userRoleViewModel = new UserRoleViewModel
+                {
+                    UserId = user.Id,
+                    UserName = user.UserName
+                
+                };
+                
+                var userInRole = await userManager.IsInRoleAsync(user, role.Name);
+                if (userInRole)
+                {
+                    userRoleViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRoleViewModel.IsSelected = false;
+                }
+
+                model.Add(userRoleViewModel);
+            }
+
+            return View(model);              
+           
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string roleId)
+        {
+            ViewBag.roleId = roleId;
+            var role = await roleManager.FindByIdAsync(roleId);
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {roleId} can't be found";
+                return View("NotFound");
+            }
+
+            for(int i=0; i< model.Count; i++)
+            {
+                var user = await userManager.FindByIdAsync(model[i].UserId);
+                var userIsInRole = await userManager.IsInRoleAsync(user, role.Name);
+                IdentityResult result = null;
+
+                if (model[i].IsSelected && !userIsInRole)
+                {
+                    result = await userManager.AddToRoleAsync(user, role.Name);
+                }
+                else if (!model[i].IsSelected && userIsInRole)
+                {
+                    result = await userManager.RemoveFromRoleAsync(user, role.Name);
+                }
+                else
+                {
+                    continue;
+                }
+
+                if (result.Succeeded)
+                {
+                    if (i < (model.Count - 1))
+                        continue;
+                    else
+                    return RedirectToAction("EditRole", new { Id = roleId });
+                }
+                
+            }
+
+            return RedirectToAction("EditRole", new { Id = roleId});
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult AccessDenied()
+        {
+            return View();
         }
     }
 }
